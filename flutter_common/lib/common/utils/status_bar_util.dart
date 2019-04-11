cfb88ae7538961c641094a033c83948957dc404b
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-04-11 23:41:31 +0800

    change status bar

--- flutter_common/lib/common/utils/status_bar_util.dart
+++ flutter_common/lib/common/utils/status_bar_util.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/services.dart' show SystemChrome;
+import 'package:flutter/cupertino.dart';
 
 //状态栏管理工具栏/
 class StatusBarUtil {
@@ -10,11 +12,23 @@ class StatusBarUtil {
   }
 
   //Set status bar color/
-  static setColor() {
+  static setColor(
+      {statusBarColor, statusBarBrightness, systemNavigationBarColor}) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent, //背景颜色
       statusBarBrightness: Brightness.dark, //图标颜色
       systemNavigationBarColor: Colors.red, //底部navigationBar背景颜色
     ));
+  }
+
+  //设置沉浸式状态栏 注:部分Android手机无效 需设置外部Activity/
+  static setImmStatusBar() {
+    if (Platform.isAndroid) {
+      // 以下两行 设置android状态栏为透明的沉浸。
+      // 写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+      SystemUiOverlayStyle systemUiOverlayStyle =
+          SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    }
   }
 }
