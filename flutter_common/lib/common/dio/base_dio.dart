9da4a02abc5b9637515f0a7d8f147897fb1fbdab
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-05-16 12:03:42 +0800

    add base http

---
+++ flutter_common/lib/common/dio/base_dio.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_common/common/http/http_index.dart';
+
+//网络请求集成类
+class BaseHttpWork<V extends Object> extends SimpleWork<V> {
+  // 解析响应数据
+  @override
+  V onExtractResult(resultData, SimpleWorkData<V> data) {
+    // TODO: implement onExtractResult
+    return null;
+  }
+
+  // 装配请求参数，data为最终要发送的参数集合，params为[Work]调用处端传入的参数列表
+  @override
+  void onFillParams(Map<String, dynamic> data, List params) {
+    // TODO: implement onFillParams
+  }
+
+  // 地址必须为完整地址，没有baseUrl支持，项目代码必须集中拼接和管理所有接口地址，便于维护和查找
+  @override
+  String onUrl(List params) {
+    // TODO: implement onUrl
+    return null;
+  }
+
+  // 使用post请求
+  @override
+  // TODO: implement httpMethod
+  HttpMethod get httpMethod => super.httpMethod;
+}
