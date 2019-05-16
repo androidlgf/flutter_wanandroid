f13cfab107b9f79d6564cf30b0d79499c2601140
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-05-16 11:41:11 +0800

    change update version

---
+++ flutter_common/lib/common/http/http_index.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_common/common/http/work_core.dart';
+export 'package:flutter_common/common/http/work_config.dart';
+export 'package:flutter_common/common/http/simple_work.dart';
+export 'package:flutter_common/common/http/communication.dart';
---
+++ flutter_common/lib/common/http/work_config.dart
@@ -0,0 +1,15 @@
+
+import 'package:dio/dio.dart';
+import 'dart:io';
+
+/// 是否开启debug模式，开启后会输出日志
+bool debugWork = true;
+
+/// 全局使用的[Dio]请求对象
+Dio dio = Dio(
+  BaseOptions(
+    connectTimeout: 30000,
+    receiveTimeout: 30000,
+    contentType: ContentType.parse("application/x-www-form-urlencoded"),
+  ),
+);
