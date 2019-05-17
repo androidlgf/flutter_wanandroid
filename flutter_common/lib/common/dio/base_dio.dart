45ed6ebfeb7191907ca9ccc6c77121e6cc93785f
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-05-17 10:17:57 +0800

    change base dio

--- flutter_common/lib/common/dio/base_dio.dart
+++ flutter_common/lib/common/dio/base_dio.dart
@@ -5,8 +5,21 @@ class BaseHttpWork<V extends Object> extends SimpleWork<V> {
   // 解析响应数据
   @override
   V onExtractResult(resultData, SimpleWorkData<V> data) {
-    // TODO: implement onExtractResult
-    return null;
+    return resultData;
+  }
+
+  @override
+  V onDefaultResult(resultData, SimpleWorkData<V> data) {
+    return resultData;
+  }
+  @override
+  bool onResponseResult(response) {
+    return true;
+  }
+  /// 解析响应体，返回解析结果
+  @override
+  bool onCheckResponse(response) {
+    return true;
   }
 
   // 装配请求参数，data为最终要发送的参数集合，params为[Work]调用处端传入的参数列表
@@ -18,7 +31,6 @@ class BaseHttpWork<V extends Object> extends SimpleWork<V> {
   // 地址必须为完整地址，没有baseUrl支持，项目代码必须集中拼接和管理所有接口地址，便于维护和查找
   @override
   String onUrl(List params) {
-    // TODO: implement onUrl
     return null;
   }
 
