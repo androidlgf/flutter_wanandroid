3800cc682282a32d6bf310e8bcbbff3b59345d48
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-05-20 11:17:43 +0800

    add HttpDefaultHeaders

--- flutter_common/lib/common/dio/base_dio.dart
+++ flutter_common/lib/common/dio/base_dio.dart
@@ -9,25 +9,29 @@ class BaseHttpWork<V extends Object> extends SimpleWork<V> {
   }
 
   @override
-  V onDefaultResult(resultData, SimpleWorkData<V> data) {
-    return resultData;
-  }
-  @override
-  bool onResponseResult(response) {
-    return true;
-  }
-  /// 解析响应体，返回解析结果
-  @override
-  bool onCheckResponse(response) {
-    return true;
+  Map<String, dynamic> onHeaders(List params) {
+    // TODO: implement onHeaders
+    return super.onHeaders(params);
   }
 
+//  @override
+//  V onDefaultResult(resultData, SimpleWorkData<V> data) {
+//    return resultData;
+//  }
+//  @override
+//  bool onResponseResult(response) {
+//    return true;
+//  }
+//  /// 解析响应体，返回解析结果
+//  @override
+//  bool onCheckResponse(response) {
+//    return true;
+//  }
   // 装配请求参数，data为最终要发送的参数集合，params为[Work]调用处端传入的参数列表
   @override
   void onFillParams(Map<String, dynamic> data, List params) {
     // TODO: implement onFillParams
   }
-
   // 地址必须为完整地址，没有baseUrl支持，项目代码必须集中拼接和管理所有接口地址，便于维护和查找
   @override
   String onUrl(List params) {
--- flutter_common/lib/common/http/http_default_headers.dart
+++ flutter_common/lib/common/http/http_default_headers.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_common/common/utils/device_util.dart';
+
 const String VERSION = 'version';
 const String USER_AGENT = 'user-agent';
 const String IP = 'ip';
@@ -7,26 +9,24 @@ const String ENCODING = 'Accept-Encoding';
 
 ///Http DefaultHeader
 class HttpDefaultHeaders {
-  Map<String, dynamic> buildDefaultHeaders() {
+  Future<Map<String, dynamic>> buildDefaultHeaders() async {
     Map<String, dynamic> defaultHeaderMap = new Map();
-    defaultHeaderMap.putIfAbsent(VERSION, () {
-      '1.0.0';
-    });
-    defaultHeaderMap.putIfAbsent(USER_AGENT, () {
-      '1.0.0';
-    });
-    defaultHeaderMap.putIfAbsent(IP, () {
-      '1.0.0';
-    });
-    defaultHeaderMap.putIfAbsent(NETWORK, () {
-      '1.0.0';
-    });
-    defaultHeaderMap.putIfAbsent(LANGUAGE, () {
-      '1.0.0';
-    });
-    defaultHeaderMap.putIfAbsent(ENCODING, () {
-      '1.0.0';
-    });
+
+    defaultHeaderMap.putIfAbsent(VERSION, () => '1.0.0');
+
+    var userAgent = await DeviceUtil.getUserAgent();
+    defaultHeaderMap.putIfAbsent(USER_AGENT, () => userAgent);
+
+    var localIPAddress = await DeviceUtil.getLocalIPAddress();
+    defaultHeaderMap.putIfAbsent(IP, () => localIPAddress);
+
+    var language = await DeviceUtil.getLanguage();
+    defaultHeaderMap.putIfAbsent(LANGUAGE, () => language);
+
+    defaultHeaderMap.putIfAbsent(NETWORK, () => '1.0.0');
+
+    defaultHeaderMap.putIfAbsent(ENCODING, () => '*');
+
     return defaultHeaderMap;
   }
 }
