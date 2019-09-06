2ad199ad7927b7e88e9a6c9a8397db7a3d17b5e8
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-09-06 11:21:23 +0800

    add route_util

--- flutter_common/lib/common/utils/route_util.dart
+++ flutter_common/lib/common/utils/route_util.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+// 跳转并清空之前任务栈所有Page
 void pushAndRemovePage(BuildContext context, Widget routePage) {
   Navigator.of(context).pushAndRemoveUntil(
     MaterialPageRoute(builder: (context) => routePage),
@@ -8,10 +9,12 @@ void pushAndRemovePage(BuildContext context, Widget routePage) {
   );
 }
 
+//跳转加入当前任务栈
 void pushNewPage(BuildContext context, Widget routePage) {
   Navigator.push(context, MaterialPageRoute(builder: (context) => routePage));
 }
 
+//跳转加入当前任务栈(👈右进左出👉动效)
 void pushNewPageBack(BuildContext context, Widget routePage) {
   Navigator.of(context)
       .push(CupertinoPageRoute(builder: (context) => routePage));
@@ -21,6 +24,7 @@ void popAndPushNewPage(BuildContext context, String routeName) {
   Navigator.popAndPushNamed(context, routeName);
 }
 
+//跳转并弹出上一个Page任务栈
 void pushReplacement(BuildContext context, Widget routePage) {
   Navigator.pushReplacement(
       context, MaterialPageRoute(builder: (context) => routePage));
